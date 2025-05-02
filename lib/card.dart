9a4940a1c1aebd2fe8ff2b257c4bf4c3519115ff
Everton Jales <everton.jales@fornecedores.sicoob.com.br>
2025-05-02 16:55:03 -0300

    card and rank classes

---
+++ lib/card.dart
@@ -0,0 +1,23 @@
+import 'package:flame/components.dart';
+import 'package:klondike/klondike_game.dart';
+import 'package:klondike/rank.dart';
+import 'package:klondike/suit.dart';
+
+class Card extends PositionComponent {
+  final Rank rank;
+  final Suit suit;
+  bool _faceUp;
+
+  Card(int intRank, int intSuit)
+    : rank = Rank.fromInt(intRank),
+      suit = Suit.fromInt(intSuit),
+      _faceUp = false,
+      super(size: KlondikeGame.cardSize);
+
+  bool get isFaceUp => _faceUp;
+  bool get isFaceDown => !_faceUp;
+  void flip() => _faceUp = !_faceUp;
+
+  @override
+  String toString() => rank.label + suit.label;
+}
